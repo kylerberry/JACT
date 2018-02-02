7390c7f748a4a94746f6ab04479ca8c9549ce377
kylerberry <kyler.berry@gmail.com>
2018-02-02 11:29:10 -0500

    add comments to strategy test

--- test/StrategyTest.js
+++ test/StrategyTest.js
@@ -5,6 +5,7 @@ let historicData = []
 
 // const generateMockHistoricData = trend => {
 //     if (trend == 'bearish') {
+
 //     } else if (trend == 'bullish') {
 
 //     } else if (trend == 'ranging') {
@@ -12,40 +13,12 @@ let historicData = []
 //     }
 // }
 
-//[time, low, high, open, close, volume]
-//time asc, older -> newer
-// let historicDataFixture = [
-//     [moment(), 100, 100, 100, 100, 30000],
-//     [moment().add(1, 'second'), 100, 100, 100, 100, 30000],
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000],
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-//     [moment().add(2, 'second'), 100, 100, 100, 100, 30000]
-// ]
-
 test('Strategy initializes with a provided strategy filename', t => {
-    const providedStrat = new Strategy({ strategy: 'macd' })
-    t.is(providedStrat instanceof Strategy, true, 'without extension');
+    const strat = new Strategy({ strategy: 'macd' })
+    t.is(strat instanceof Strategy, true, 'without extension');
 
-    // this test is brittle because it relies on an external file source to be there
-    const customStrat = new Strategy({ strategy: 'private.strategy' })
-    t.is(customStrat instanceof Strategy, true, 'without extension');
-
-    const providedStratDotJs = new Strategy({ strategy: 'macd.js' })
-    t.is(providedStratDotJs instanceof Strategy, true, 'with extension');
-
-    // this test is brittle because it relies on an external file source to be there
-    const customStratDotJs = new Strategy({ strategy: 'private.strategy.js' })
-    t.is(customStratDotJs instanceof Strategy, true, 'with extension');
+    const stratDotJs = new Strategy({ strategy: 'rsi.js' })
+    t.is(stratDotJs instanceof Strategy, true, 'with extension');
 })
 
 test('Strategy throws error if valid strategy name or path is not provided', t => {
@@ -56,6 +29,7 @@ test('Strategy throws error if valid strategy name or path is not provided', t =
     t.truthy(err)
 })
 
+// @todo
 test('Strategy.signal returns `LONG`, `SHORT`, or null', t => {
     const strat = new Strategy({ strategy: 'macd'})
     t.pass()
