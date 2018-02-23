2715f0760e2b938b19960f6b0d1168f31ad4f9ca
kylerberry <kyler.berry@gmail.com>
2018-02-23 18:06:43 -0500

    add tests for new configProvider

---
+++ test/ConfigProviderTest.js
@@ -0,0 +1,56 @@
+const test = require('ava');
+const config = require('../lib/ConfigProvider')
+
+let configPath = './test/fixtures/config.yaml'
+
+let defaults =  {
+	gdax_auth_secret: 'someSecret',
+	gdax_auth_key: 'someKey',
+	gdax_auth_passphrase: 'somePassphrase',
+	strategy: 'macd',
+	granularity: 900,
+	product: 'BTC-USD',
+	stop_loss: null,
+	allowed_slippage: null,
+	max_funds: null,
+	logging: false
+}
+
+test('Config initializes from a config.yaml file', t => {
+	config._setConfigPath(configPath)
+	config.initFromFile()
+	t.is(config.get('product'), 'LTC-USD')
+})
+
+test('Validate a config object', t => {
+	let badConfig = Object.assign({}, defaults)
+	delete badConfig.gdax_auth_secret
+	// throws on bad config
+	t.truthy(t.throws(() => {
+		config._validate(badConfig)
+	}, Error))
+
+	// true on good config
+	t.truthy(config._validate(defaults))
+})
+
+test('Set config', t => {
+	//initial setup
+	config.set(config)
+
+	// set by key, value
+	config.set('product', 'ETH-USD')
+	t.is(config.get('product'), 'ETH-USD')
+
+	// set by object
+	config.set({
+		strategy: 'rsi'
+	})
+	t.is(config.get('strategy'), 'rsi')
+
+	// config unchanged if an invalid option is passed
+	t.is(config.set({
+		gdax_auth_key: null
+	}), false)
+	t.is(config.get('gdax_auth_key'), 'someKey')
+})
